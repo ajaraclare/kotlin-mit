7d04566614a9a4e2c441392cc1bcdbf538cf65ab
Clair <clareajara@gmail.com>
2023-08-15 20:21:00 +0300

    Modifications

---
+++ src/main/kotlin/assignment.kt
@@ -0,0 +1,20 @@
+fun main (){
+    operation()
+
+}
+
+fun operation(){
+
+    var integer = 0
+
+    if(integer < 0){
+        println("The number is negative")
+    }
+    else if(integer > 0){
+        println("The number is positive")
+    }
+    else{
+        println("The number is zero")
+    }
+
+}
---
+++ src/main/kotlin/evenandodd.kt
@@ -0,0 +1,17 @@
+fun main(){
+    remainder()
+
+}
+
+fun remainder(){
+    var number = 34
+
+    if(number % 2 == 0){
+        println("$number is even")
+    }
+    else{
+        println("$number is odd")
+    }
+
+
+}
